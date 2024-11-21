63e32e97045e91f9cd44ca3ecf7413ab2fdb424f
samwhelp <samwhelp@users.noreply.github.com>
2024-11-21 13:55:14 +0800

    fix

--- cover.sh
+++ cover.sh
@@ -1,1 +1,53 @@
 #!/usr/bin/env bash
+
+
+
+
+##
+## # Cover
+##
+
+
+##
+## ## Main / Init
+##
+
+REF_BASE_DIR_PATH="$(cd -- "$(dirname -- "$0")" ; pwd)"
+REF_CMD_FILE_NAME="$(basename "$0")"
+
+
+DEFAULT_IS_DEBUG="false"
+IS_DEBUG="${IS_DEBUG:=$DEFAULT_IS_DEBUG}"
+
+
+##
+## ## Main / Util
+##
+
+util_error_echo () {
+	echo "${@}" 1>&2
+}
+
+util_debug_echo () {
+
+	if is_debug; then
+		echo "${@}" 1>&2
+	fi
+
+}
+
+is_debug () {
+
+	if [[ "${IS_DEBUG}" == "true" ]]; then
+		return 0
+	fi
+
+	return 1
+
+}
+
+is_not_debug () {
+
+	! is_debug
+
+}
