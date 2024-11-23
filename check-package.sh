95cfc9eed62d7b2e5162c64d924cb5601e8756ff
samwhelp <samwhelp@users.noreply.github.com>
2024-11-23 14:39:33 +0800

    fix

---
+++ check-package.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+##
+## https://github.com/Nitrux/nx-desktop/blob/legacy-plasma/gh-build.sh#L8C1-L8C78
+## ``` sh
+## deps=$(sed -e '/^#.*$/d; /^$/d; /^\s*$/d' package/dependencies | paste -sd ,)
+## ```
+##
+mkdir -p tmp
+cat asset/package/install/* > tmp/package-list.txt
+package_list=$(sed -e '/^#.*$/d; /^$/d; /^\s*$/d' tmp/package-list.txt | paste -sd ' ')
+
+##echo ${package_list}
+sudo apt install ${package_list}
+
